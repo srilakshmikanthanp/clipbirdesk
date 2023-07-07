d2cc421f2cb554c06839156dbe7bec31613f0129
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2023-07-07 16:50:43 +0530

    Update: Change folder structure & varies_t

--- tests/utility/functions/coding.hpp
+++ tests/utility/functions/coding.hpp
@@ -8,7 +8,7 @@
 #include <gtest/gtest.h>
 
 #include "../../../utility/functions/coding.hpp"
-
+#include "../../../types/types.hpp"
 /**
  * @brief testing netToHost function with different types
  * int64_t, int32_t, int16_t, int8_t
@@ -83,27 +83,30 @@ TEST(ToBytesTest, TestingToBytes) {
   // using the toBytes function
   using srilakshmikanthanp::clipbirdesk::utility::functions::coding::toBytes;
 
+  // using varies_t type
+  using varies_t = srilakshmikanthanp::clipbirdesk::types::varies_t;
+
   // int64_t
   int64_t i64_t = 0x123456789ABCDEF0;
-  std::vector<std::uint8_t> o64_t = {0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12};
+  varies_t o64_t = {0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12};
 
   EXPECT_EQ(toBytes(i64_t), o64_t);
 
   // int16_t
   int16_t i16_t = 0x1234;
-  std::vector<std::uint8_t> o16_t = {0x34, 0x12};
+  varies_t o16_t = {0x34, 0x12};
 
   EXPECT_EQ(toBytes(i16_t), o16_t);
 
   // int8_t
   int8_t i8_t = 0x12;
-  std::vector<std::uint8_t> o8_t = {0x12};
+  varies_t o8_t = {0x12};
 
   EXPECT_EQ(toBytes(i8_t), o8_t);
 
   // int32_t
   int32_t i32_t = 0x12345678;
-  std::vector<std::uint8_t> o32_t = {0x78, 0x56, 0x34, 0x12};
+  varies_t o32_t = {0x78, 0x56, 0x34, 0x12};
 
   EXPECT_EQ(toBytes(i32_t), o32_t);
 }
@@ -116,26 +119,29 @@ TEST(FromBytesTest, TestingFromBytes) {
   // using the fromBytes function
   using srilakshmikanthanp::clipbirdesk::utility::functions::coding::fromBytes;
 
+  // using varies_t type
+  using varies_t = srilakshmikanthanp::clipbirdesk::types::varies_t;
+
   // int64_t
-  std::vector<std::uint8_t> i64_t = {0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12};
+  varies_t i64_t = {0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12};
   int64_t o64_t = 0x123456789ABCDEF0;
 
   EXPECT_EQ(fromBytes<int64_t>(i64_t), o64_t);
 
   // int16_t
-  std::vector<std::uint8_t> i16_t = {0x34, 0x12};
+  varies_t i16_t = {0x34, 0x12};
   int16_t o16_t = 0x1234;
 
   EXPECT_EQ(fromBytes<int16_t>(i16_t), o16_t);
 
   // int8_t
-  std::vector<std::uint8_t> i8_t = {0x12};
+  varies_t i8_t = {0x12};
   int8_t o8_t = 0x12;
 
   EXPECT_EQ(fromBytes<int8_t>(i8_t), o8_t);
 
   // int32_t
-  std::vector<std::uint8_t> i32_t = {0x78, 0x56, 0x34, 0x12};
+  varies_t i32_t = {0x78, 0x56, 0x34, 0x12};
   int32_t o32_t = 0x12345678;
 
   EXPECT_EQ(fromBytes<int32_t>(i32_t), o32_t);
