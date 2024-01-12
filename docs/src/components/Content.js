62aaab4082f955096e02bade0c5438af8486cbfb
Prahas Tech Interns <interns@prahastechnologies.com>
2024-01-12 09:47:43 +0530

    UI Updated

---
+++ docs/src/components/Content.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Content.css';
+
+const Content = () => {
+  return (
+    <div class="content">
+        <p class="desc">
+            &#8220; A Bird that syncs your clipboard data between devices connected in a local network.&#8221;
+        </p>
+        <div class="youtubeVideo">
+            <iframe width="100%" height="100%"
+                src="https://www.youtube.com/embed/Hu4Yvq-g7_Y" 
+                title="How to Get Your Brain to Focus | Chris Bailey | TEDxManchester" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen>
+            </iframe>
+        </div>
+    </div>
+  )
+}
+
+export default Content
