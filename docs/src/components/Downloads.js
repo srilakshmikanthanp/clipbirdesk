8267673c3fdc252defcc5c13fd15bc1bd443cf2b
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2024-01-12 12:10:43 +0530

    Delete unused files and update styles

--- docs/src/components/Downloads.js
+++ docs/src/components/Downloads.js
@@ -7,26 +7,20 @@ import linux from '../images/linux.png';
 const Downloads = () => {
   return (
     <div className='downloads'>
-        <div className="downloadWrap">
-            <div className="os windows">
-                <img className="image" src={windows} />
-                <h2 className='osName'>Windows</h2>
-                <p className="osDesc">Support x64 windows 8.1 or above</p>
-                <button class="downloadButton" >Download</button>
-            </div>
-            <div className="os android">
-                <img className="image" src={android} />
-                <h2 className='osName'>Android</h2>
-                <p className="osDesc">Sync clipboard across all your android devices</p>
-                <button class="downloadButton androidButton" >Download</button>
-            </div>
-            <div className="os linux">
-                <img className="image" src={linux} />
-                <h2 className='osName'>Linux</h2>
-                <p className="osDesc">Sync Clipboard across your linux systems</p>
-                <button class="downloadButton" >Download</button>
-            </div>
+      <div className="downloadWrap">
+        <div className="os windows">
+          <img className="image" src={windows} />
+          <h2 className='osName'>Windows</h2>
+          <p className="osDesc">Support x64 windows from 10 or above</p>
+          <a class="downloadButton" href="https://github.com/srilakshmikanthanp/clipbirdesk/releases">Download</a>
         </div>
+        <div className="os android">
+          <img className="image" src={android} />
+          <h2 className='osName'>Android</h2>
+          <p className="osDesc">Supports Android from version 9.0 or above</p>
+          <a class="downloadButton" href="https://github.com/srilakshmikanthanp/clipbirdroid/releases">Download</a>
+        </div>
+      </div>
     </div>
   )
 }
