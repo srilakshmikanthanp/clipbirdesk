f5b5580781ba5edf3130fc7e290a8e96018bb146
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2024-01-20 20:44:43 +0530

    Remove a logo from Header

--- docs/src/components/Header.jsx
+++ docs/src/components/Header.jsx
@@ -12,11 +12,6 @@ const Header = () => {
         <div className="logo-container">
           <img src={logo} alt="Clipbird" className="logo" style={{ width: "50px", height: "50px" }}/>
         </div>
-        <div className="logo">
-        <a href="https://srilakshmikanthanp.github.io/" target="_blank" rel="noopener noreferrer">
-            <img src={icon} alt="github" className="slk" style={{ width: "40px", height: "40px", }}/>
-          </a>
-        </div>
         <div className="gitlink">
           <a href="https://github.com/srilakshmikanthanp/clipbirdesk" target="_blank" rel="noopener noreferrer">
             <img src={github} alt="github" className="github" style={{ width: "40px", height: "40px" }}/>
