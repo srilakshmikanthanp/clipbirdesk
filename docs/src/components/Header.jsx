e16fc146d4bdfb92357598c479c01b94a0cea856
manojrajm <gauthamtamziha007@gmail.com>
2024-01-20 17:33:59 +0530

    add responsive

--- docs/src/components/Header.jsx
+++ docs/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../images/logo.png";
 import github from "../images/github.png";
+import icon from "../images/slk.png";
+
 
 import "./Header.css";
 
@@ -9,6 +11,11 @@ const Header = () => {
     <nav id="nav">
         <div className="logo-container">
           <img src={logo} alt="Clipbird" className="logo" style={{ width: "50px", height: "50px" }}/>
+        </div>
+        <div className="logo">
+        <a href="https://srilakshmikanthanp.github.io/" target="_blank" rel="noopener noreferrer">
+            <img src={icon} alt="github" className="slk" style={{ width: "40px", height: "40px", }}/>
+          </a>
         </div>
         <div className="gitlink">
           <a href="https://github.com/srilakshmikanthanp/clipbirdesk" target="_blank" rel="noopener noreferrer">
