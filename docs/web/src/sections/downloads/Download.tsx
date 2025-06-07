44f81b64e07152364badb02dbadea80e29379e98
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-06-07 10:54:50 +0530

    Add seperate button for each platform

--- docs/web/src/sections/downloads/Download.tsx
+++ docs/web/src/sections/downloads/Download.tsx
@@ -1,23 +1,34 @@
 import { BsAndroid2 } from 'react-icons/bs';
 import { ImWindows } from 'react-icons/im';
-import { MdOutlineFileDownload } from 'react-icons/md';
+import { FaLinux } from 'react-icons/fa';
 import Button from '../../components/button/Button';
 import classes from './download.module.css';
 
 export default function Download() {
-    return (
-        <section className={classes.download}>
-            <h2>Downloads</h2>
-            <p>Download the latest version for your platform from the GitHub releases.</p>
-            <p className={classes.description}>
-                ClipBird is currently available for Windows <ImWindows size={20} /> and Android <BsAndroid2 size={20} />
-            </p>
-            <Button
-                icon={<MdOutlineFileDownload size={20} />}
-                href="https://github.com/srilakshmikanthanp/clipbirdroid/releases/latest"
-            >
-                Download
-            </Button>
-        </section>
-    );
+  return (
+    <section className={classes.download} id="downloads">
+      <h2>Downloads</h2>
+      <p>Download the latest version for your platform from the GitHub releases.</p>
+      <div className={classes.links}>
+        <Button
+          icon={<ImWindows size={20} />}
+          href="https://github.com/srilakshmikanthanp/clipbirdesk/releases/latest"
+        >
+          Windows
+        </Button>
+        <Button
+          icon={<FaLinux size={20} />}
+          href="https://github.com/srilakshmikanthanp/clipbirdesk/releases/latest"
+        >
+          Linux
+        </Button>
+        <Button
+          icon={<BsAndroid2 size={20} />}
+          href="https://github.com/srilakshmikanthanp/clipbirdroid/releases/latest"
+        >
+          Andrpid
+        </Button>
+      </div>
+    </section>
+  );
 }
