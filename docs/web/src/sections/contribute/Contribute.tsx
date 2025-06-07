d73399cd29b3e995ba9a96a767a3060262111cef
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-06-07 11:02:46 +0530

    Enhance contribution section with platform-specific buttons

--- docs/web/src/sections/contribute/Contribute.tsx
+++ docs/web/src/sections/contribute/Contribute.tsx
@@ -6,90 +6,89 @@ import Button from '../../components/button/Button';
 import type { IStep } from '../../components/steps/IStep';
 import StepList from '../../components/steps/StepList';
 import classes from './contribute.module.css';
+import { FaDesktop } from 'react-icons/fa';
+import { BsAndroid } from 'react-icons/bs';
 
 const contributeSteps: IStep[] = [
-    {
-        step_number: 1,
-        icon: <VscRepoForked size={50} color="var(--color-secondary)" />,
-        title: 'Fork the Repository',
-        description: 'Start by forking the ClipBird repository on GitHub to create your copy of the project.',
-        cta: (
-            <Button icon={<VscRepoForked />} href="https://github.com/srilakshmikanthanp/clipbirdesk">
-                Fork
-            </Button>
-        )
-    },
-    {
-        step_number: 2,
-        icon: <VscGitPullRequestGoToChanges size={50} color="var(--color-secondary)" />,
-        title: 'Make Changes',
-        description: 'Make the desired changes, add new features, or fix bugs in your forked repository.'
-    },
-    {
-        step_number: 3,
-        icon: <IoIosGitPullRequest size={50} color="var(--color-secondary)" />,
-        title: 'Create a PR',
-        description:
-            'Submit a pull request to the main ClipBird repository. Provide a clear description of your changes.',
-        cta: (
-            <Button icon={<IoIosGitPullRequest />} href="https://github.com/srilakshmikanthanp/clipbirdesk/compare">
-                Submit PR
-            </Button>
-        )
-    }
+  {
+    step_number: 1,
+    icon: <VscRepoForked size={50} color="var(--color-secondary)" />,
+    title: 'Fork the Repository',
+    description: 'Start by forking the ClipBird repository on GitHub to create your copy of the project.',
+    cta: (
+      <div className={classes.contribute__forks}>
+        <Button icon={<FaDesktop size={20} />} href="https://github.com/srilakshmikanthanp/clipbirdesk" />
+        <Button icon={<BsAndroid />} href="https://github.com/srilakshmikanthanp/clipbirdroid" />
+      </div>
+    )
+  },
+  {
+    step_number: 2,
+    icon: <VscGitPullRequestGoToChanges size={50} color="var(--color-secondary)" />,
+    title: 'Make Changes',
+    description: 'Make the desired changes, add new features, or fix bugs in your forked repository.'
+  },
+  {
+    step_number: 3,
+    icon: <IoIosGitPullRequest size={50} color="var(--color-secondary)" />,
+    title: 'Create a PR',
+    description:
+      'Submit a pull request to the main ClipBird repository. Provide a clear description of your changes.',
+  }
 ];
 
 const supportSteps: IStep[] = [
-    {
-        step_number: 1,
-        icon: <LuStar size={50} color="var(--color-secondary)" />,
-        title: 'Star the Repository',
-        description: 'Copy text, images, or emojis on one device and seamlessly paste them into another device.',
-        cta: (
-            <Button icon={<LuStar />} href="https://github.com/srilakshmikanthanp/clipbirdesk">
-                Star
-            </Button>
-        )
-    },
-    {
-        step_number: 2,
-        icon: <BiDonateHeart size={50} color="var(--color-secondary)" />,
-        title: 'Sponsor the Project',
-        description: 'If you love ClipBird and want to support its development, consider becoming a sponsor.',
-        cta: (
-            <Button icon={<BiDonateHeart />} href="https://donate.srilakshmikanthanp.com/">
-                Donate
-            </Button>
-        )
-    }
+  {
+    step_number: 1,
+    icon: <LuStar size={50} color="var(--color-secondary)" />,
+    title: 'Star the Repository',
+    description: 'Copy text, images, or emojis on one device and seamlessly paste them into another device.',
+    cta: (
+      <div className={classes.contribute__stars}>
+        <Button icon={<FaDesktop size={20} />} href="https://github.com/srilakshmikanthanp/clipbirdesk" />
+        <Button icon={<BsAndroid />} href="https://github.com/srilakshmikanthanp/clipbirdroid" />
+      </div>
+    )
+  },
+  {
+    step_number: 2,
+    icon: <BiDonateHeart size={50} color="var(--color-secondary)" />,
+    title: 'Sponsor the Project',
+    description: 'If you love ClipBird and want to support its development, consider becoming a sponsor.',
+    cta: (
+      <Button icon={<BiDonateHeart />} href="https://donate.srilakshmikanthanp.com/">
+        Donate
+      </Button>
+    )
+  }
 ];
 
 export default function Contribute() {
-    return (
-        <section className={classes.contribute}>
-            <h2>Contribute</h2>
-            <p>
-                ClipBird is a free and open-source project, and we welcome contributions from the community. Whether you
-                want to change, add a feature, or fix a bug, your contributions are valuable to us.
-            </p>
+  return (
+    <section className={classes.contribute}>
+      <h2>Contribute</h2>
+      <p>
+        ClipBird is a free and open-source project, and we welcome contributions from the community. Whether you
+        want to change, add a feature, or fix a bug, your contributions are valuable to us.
+      </p>
 
-            <div className={classes.contribute__option}>
-                <h4>Here's how you can contribute:</h4>
+      <div className={classes.contribute__option}>
+        <h4>Here's how you can contribute:</h4>
 
-                <div className={classes.contribute__steps}>
-                    <StepList steps={contributeSteps} />
-                </div>
-            </div>
+        <div className={classes.contribute__steps}>
+          <StepList steps={contributeSteps} />
+        </div>
+      </div>
 
-            <hr className={classes.contribute__separator} />
+      <hr className={classes.contribute__separator} />
 
-            <div className={classes.contribute__option}>
-                <h4>Here are some other ways to support the project:</h4>
+      <div className={classes.contribute__option}>
+        <h4>Here are some other ways to support the project:</h4>
 
-                <div className={classes.contribute__steps}>
-                    <StepList steps={supportSteps} />
-                </div>
-            </div>
-        </section>
-    );
+        <div className={classes.contribute__steps}>
+          <StepList steps={supportSteps} />
+        </div>
+      </div>
+    </section>
+  );
 }
