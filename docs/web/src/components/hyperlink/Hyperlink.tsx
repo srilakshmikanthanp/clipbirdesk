93b701f34c64f91fc12e3cf1c15804fe76eb27cb
krissh-the-dev <akrishnamoorthy007@gmail.com>
2025-06-07 22:53:47 +0530

    feat(web/components): added hyperlink component

---
+++ docs/web/src/components/hyperlink/Hyperlink.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from 'react';
+import classes from './hyperlink.module.css'
+
+export default function Hyperlink(props: IHyperlinkProps) {
+    return (<a className={classes.hyperlink} href={props.href}>{props.icon}{props.text}</a>);
+}
+
+interface IHyperlinkProps {
+    icon?: JSX.Element;
+    text?: string;
+    href?: string;
+}
