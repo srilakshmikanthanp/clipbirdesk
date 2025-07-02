adf41195b52ce9a86d07a97dca19c095b2fe0cff
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-07-02 20:30:25 +0530

    Improve logging message format and default values

--- src/utility/logging/logging.cpp
+++ src/utility/logging/logging.cpp
@@ -8,14 +8,14 @@ void Logger::handler(QtMsgType type, const QMessageLogContext &context, const QS
   // if log file is not set
   if (!logs) throw std::runtime_error("Log file is not set");
 
-  auto *function = context.function ? context.function : "";
-  auto *file     = context.file ? context.file : "";
-  auto message   = QObject::tr("%1 | %2 | at line : %3 : %4 : %5()-> %6\n")
+  auto *function = context.function ? context.function : "<unknown function>";
+  auto *file     = context.file ? context.file : "<unknown file>";
+  auto message   = QObject::tr("[%1][%2][%3][%4:%5]: %6")
                      .arg(QDateTime::currentDateTime().toString("dd-MM-yyyy hh:mm:ss"))
-                     .arg(Logger::contextNames.value(type))
-                     .arg(context.line)
+                     .arg(Logger::contextNames.value(type).trimmed())
                      .arg(QFileInfo(QString(file)).baseName())
                      .arg(QString(function))
+                     .arg(context.line)
                      .arg(msg)
                      .toLocal8Bit();
 
